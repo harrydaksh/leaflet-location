1ad1d3c727b991c034a290b27c9f166b583886a1
Harinder daksh <81361432+harrydaksh@users.noreply.github.com>
2022-09-29 12:36:37 +0530

    live location

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-var mymap = L.map('my_map').setView([12.9063861, 77.6108759], 5);
+
+var mymap = L.map('my_map').setView([12.9063861, 77.6108759], 6);
 const tileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
 
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
 const tileLayers = L.tileLayer(tileUrl,{attribution})
 tileLayers.addTo(mymap)
 
@@ -86,7 +84,43 @@ setTimeout(()=>{
     .setLatLng([lat,lng])
     .setContent(makePopupContent(store))
     .openOn(mymap)
-},3000)
+},3000
+)}
 
 
+
+//locate my location
+
+mymap.locate({setView: true, maxZoom: 16});
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(mymap)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(mymap);
 }
+
+mymap.on('locationfound', onLocationFound);
+
+
+
+// mymap.createPane('labels');
+// mymap.getPane('labels').style.zIndex = 650;
+// mymap.getPane('labels').style.pointerEvents = 'none';
+// var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+//         attribution: '©OpenStreetMap, ©CartoDB'
+// }).addTo(mymap);
+
+// var positronLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
+//         attribution: '©OpenStreetMap, ©CartoDB',
+//         pane: 'labels'
+// }).addTo(mymap);
+
+// var geojson = L.geoJson(GeoJsonData, geoJsonOptions).addTo(mymap);
+
+// geojson.eachLayer(function (layer) {
+//     layer.bindPopup(layer.feature.properties.name);
+// });
+
+// mymap.fitBounds(geojson.getBounds("vhvjb"));
